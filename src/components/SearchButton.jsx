9500b83c55b87af3fb623290dda94d260b108f37
arfath-ali <1290sfaa28@gmail.com>
2025-04-17 17:03:05 +0530

    update files

--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -1,10 +1,12 @@
+import searchIcon from '../assets/images/icons/search.svg';
+
 const SearchButton = ({ onClick }) => {
   return (
     <button
       className="rounded-r-lg border border-l-0 border-gray-300 bg-[#f5f5f5] px-4 py-2.5 hover:cursor-pointer"
       onClick={onClick}>
       <img
-        src="src/assets/images/icons/search.svg"
+        src={searchIcon}
         alt="Search icon"
         className="h-5 w-5 hover:cursor-pointer"
       />
